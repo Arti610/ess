bbeedb0a2c60584b5f530744e2a95e495d1a6061
Arti610 <artinirmalkar06@gmail.com>
2024-02-03 13:17:30 +0530

    Checkin Staffs list render successfully, Not In Office staff list render successfully

--- src/pages/management/Home/CheckInUser.jsx
+++ src/pages/management/Home/CheckInUser.jsx
@@ -1,10 +1,43 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import getApi from "../../../redux/slices/utils/getApi";
+import UserCard from "../../../utils/UserCard";
+import Loader from "../../../utils/ActivityIndicator";
 
 const CheckInUser = () => {
-    return (
-        <View><Text>CheckIn User</Text></View>
-    )
-}
+    const route = useRoute();
+    const { id } = route.params;
 
-export default CheckInUser
+    const [userData, setUserData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const res = await getApi.getStaffList(id);
+                if (res.status === 200) {
+                    const filteredUserData = res.data.filter(user => user.status === 'In Office');
+                    setUserData(filteredUserData);
+                }
+            } catch (error) {
+                console.log('User Error during fetch active user', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [id]); 
+
+    return (
+        loading ? <Loader /> : (
+            <ScrollView>
+                <UserCard item={userData} id={id} />
+            </ScrollView>
+        )
+    );
+};
+
+export default CheckInUser;
--- src/pages/management/Home/NotCheckInUser.jsx
+++ src/pages/management/Home/NotCheckInUser.jsx
@@ -3,33 +3,40 @@ import { ScrollView, Text, View } from "react-native";
 import UserCard from "../../../utils/UserCard";
 import { useRoute } from "@react-navigation/native";
 import getApi from "../../../redux/slices/utils/getApi";
+import Loader from "../../../utils/ActivityIndicator";
 
 const NotCheckInUser = () => {
     const route = useRoute();
     const { id } = route.params;
 
     const [userData, setUserData] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
+                setLoading(true)
                 const res = await getApi.getStaffList(id)
                 if (res.status = 200) {
                     const filteredUserData = res.data.filter(user => user.status == 'Not In Office');
                     setUserData(filteredUserData);
+                    setLoading(false)
                 }
             }
             catch (error) {
+                setLoading(false)
                 console.log('User Errror during fetch active user', error);
             }
         }
         fetchData()
-    }, [])
+    }, [id])
 
     return (
-        <ScrollView>
-            <UserCard item={userData} id={id} />
-        </ScrollView>
+        loading ? <Loader /> : (
+            <ScrollView>
+                <UserCard item={userData} id={id} />
+            </ScrollView>
+        )
     )
 }
 
