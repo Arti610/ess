426a119238a23de55d3113dd4ec04771fb5c72d7
Arti610 <artinirmalkar06@gmail.com>
2024-02-07 18:29:14 +0530

    UI : staff dashboard

--- src/pages/staff/dashboard/Dashboard.jsx
+++ src/pages/staff/dashboard/Dashboard.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { primaryColor, styles, textColor } from "../../../../style";
+import { primaryColor, secondaryColor, styles, textColor } from "../../../../style";
 import Icon from 'react-native-vector-icons/AntDesign'
 import getApi from "../../../redux/slices/utils/getApi";
 import { currentUser } from "../../../utils/currentUser";
+import moment from "moment";
+import { useNavigation } from "@react-navigation/native";
+import Loader from "../../../utils/ActivityIndicator";
 const Dashboard = () => {
+    const navigation = useNavigation()
     const currentDate = new Date();
     const options = { hour: '2-digit', minute: '2-digit', hour12: true };
     const formattedTime = currentDate.toLocaleTimeString('en-US', options);
 
     const [inoutData, setInoutData] = useState(null)
     const [token, setToken] = useState(null)
-    console.log('inoutData', inoutData);
-    console.log('token', token);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
 
@@ -27,9 +30,11 @@ const Dashboard = () => {
 
         const fetchData = async () => {
             try {
+                setLoading(true)
                 const res = await getApi.getAllCheckinoutList(token);
-                console.log('res.data', res);
+
                 if (res.data) {
+                    setLoading(false)
                     setInoutData(res.data);
                 }
             } catch (error) {
@@ -40,7 +45,7 @@ const Dashboard = () => {
     }, [])
 
     return (
-        <View style={style.container}>
+        loading ? <Loader /> : <View style={style.container}>
             {/*Header @start */}
             <View style={style.header}>
                 <TouchableOpacity style={style.card} >
@@ -48,14 +53,14 @@ const Dashboard = () => {
                         <View><Text style={[styles.lable, { fontSize: 17 }]}>{formattedTime}</Text></View>
                         <View><Text >checkin</Text></View>
                     </View>
-                    <View><Icon name='export' style={style.userIcon} /></View>
+                    <View><Icon name='export'style={[style.userIcon, { backgroundColor: secondaryColor}]}/></View>
                 </TouchableOpacity>
                 <TouchableOpacity style={style.card} >
                     <View style={{ justifyContent: 'center' }}>
                         <View><Text style={[styles.lable, { fontSize: 17 }]}>{formattedTime}</Text></View>
                         <View><Text >checkout</Text></View>
                     </View>
-                    <View><Icon name='export2' style={style.userIcon} /></View>
+                    <View><Icon name='export2' style={[style.userIcon, { backgroundColor: 'pink'}]} /></View>
                 </TouchableOpacity>
             </View>
             {/*Header @end */}
@@ -68,11 +73,34 @@ const Dashboard = () => {
             <View style={style.footer}>
                 <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
                     <Text style={styles.textSubHeading}>Recent Activity</Text>
-                    <Text style={{ fontSize: 12, fontWeight: 'bold' }}>View All</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('Clock')}><Text style={{ fontSize: 12, fontWeight: 'bold' }}>View All</Text></TouchableOpacity>
                 </View>
 
-                {inoutData && inoutData.check_in && inoutData.check_in.map((item) => (
-                    <View><Text>{item && item.date_time ? item.date_time : null}</Text></View>
+                {inoutData && inoutData.check_in && inoutData.check_in.slice(0, 1).map((item) => (
+                    <View style={style.activityCard}>
+                        <View><Icon name='export' style={[style.userIcon, { backgroundColor: secondaryColor}]} /></View>
+                        <View>
+                            <Text style={styles.lable}>Checkin</Text>
+                            <Text>{item.date_time ? moment(item.date_time).format('DD MMM YYYY') : null}</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.lable}>{item.date_time ? item.date_time : null}</Text>
+                            <Text>{item.punctuality}</Text>
+                        </View>
+                    </View>
+                ))}
+                {inoutData && inoutData.check_out && inoutData.check_out.slice(0, 1).map((item) => (
+                    <View style={style.activityCard}>
+                        <View><Icon name='export2' style={[style.userIcon, { backgroundColor: 'pink'}]} /></View>
+                        <View>
+                            <Text style={styles.lable}>Checkout</Text>
+                            <Text>{item.date_time ? moment(item.date_time).format('DD MMM YYYY') : null}</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.lable}>{item.date_time ? item.date_time : null}</Text>
+                            <Text>{item.punctuality}</Text>
+                        </View>
+                    </View>
                 ))}
             </View>
             {/*Footer @end */}
@@ -99,12 +127,11 @@ const style = StyleSheet.create({
     },
     body: {
         flex: 2,
-        backgroundColor: 'red'
+        // backgroundColor: 'red'
     },
     footer: {
         flex: 2,
-        backgroundColor: 'pink'
-
+        gap: 8
     },
     card: {
         width: 150,
@@ -113,8 +140,9 @@ const style = StyleSheet.create({
         elevation: 1,
         backgroundColor: 'white',
         borderColor: textColor,
-        padding: 15,
         flexDirection: 'row',
+        padding: 15,
+
         alignItems: 'start',
         justifyContent: 'space-between',
 
@@ -123,6 +151,21 @@ const style = StyleSheet.create({
     userIcon: {
         fontSize: 25,
         textAlign: 'center',
-        color: primaryColor
+        color: primaryColor,
+        padding: 8, height: 40, width: 40, borderRadius: 20
     },
+
+    activityCard: {
+        width: '100%',
+        height: 70,
+        borderRadius: 8,
+        elevation: 1,
+        backgroundColor: 'white',
+        borderColor: textColor,
+        padding: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+
+    }
 })  
